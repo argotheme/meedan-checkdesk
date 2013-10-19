0a73d4c95b8a427f9d197fb296223ad894539c11
Caio SBA <caiosba@gmail.com>
2013-10-19 17:47:31 -0300

    Adding geotag and full-width tweet

--- drupal/sites/all/themes/checkdesk/templates/oembed.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/oembed.tpl.php
@@ -11,6 +11,10 @@
   }
 ?>
 <div class="<?php print $classes . ' ' . $provider_class_name; ?>"<?php print $attributes; ?>>
+  <?php if (isset($embed->geo) && is_array($embed->geo->coordinates)): ?>
+    <p class="embed-geo-data"><?php print l('<i class="icon-pushpin"></i> ' . implode(', ', $embed->geo->coordinates), 'http://maps.google.com/?ie=UTF8&hq=&ll=' . $embed->geo->coordinates[0] . ',' . $embed->geo->coordinates[1] . '&z=13', array('html' => TRUE, 'attributes' => array('target' => '_blank'))); ?></p>
+  <?php endif; ?> 
+
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
     <a href="<?php print $original_url; ?>"<?php print $title_attributes; ?>><?php print render($title); ?></a>
